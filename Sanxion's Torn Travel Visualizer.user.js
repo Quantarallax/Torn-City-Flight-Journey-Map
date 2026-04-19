14edbd025f1b56859fce0ca02ff873edba437650
Richard Hawkins <41966714+Quantarallax@users.noreply.github.com>
2026-04-19 14:25:21 +0100

    Update Sanxion's Torn Travel Visualizer.user.js
    23.0.0

--- Sanxion's Torn Travel Visualizer.user.js
+++ Sanxion's Torn Travel Visualizer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TORN CITY Flight Visualiser
 // @namespace    sanxion.tc.flightvisualiser
-// @version      21.0.0
+// @version      23.0.0
 // @description  Real-time animated flight visualiser for Torn City. SVG world map, curved animated flight path, plane animation, ATC commentary and live flight stats.
 // @author       Sanxion [2987640]
 // @match        https://www.torn.com/page.php?sid=travel*
@@ -931,7 +931,7 @@ ${dots}
   <div id="tcfv-cred" class="tcfv-pg" style="display:none">
     <h3>&#9733; Credits</h3>
     <p class="big-t">TORN CITY<br>Flight Visualiser</p>
-    <p class="ver-t">Version 21.0.0</p>
+    <p class="ver-t">Version 23.0.0</p>
     <p>Designed &amp; developed by</p>
     <a href="https://www.torn.com/profiles.php?XID=2987640" target="_blank" id="tcfv-author">&#9992; Sanxion [2987640]</a>
     <hr>
